2541fb400beb9784a6fe4b0d1c0fbfba23d4923d
Shamiul40 <shamiul4040@gmail.com>
2025-12-01 22:29:34 +0600

    sidebar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.css'
-import Header from './Components/Header'
+import "./App.css";
+import Header from "./Components/Header";
+import SideBar from "./Components/SideBar";
 
 function App() {
- 
   return (
     <>
-     <Header></Header>
+      <Header></Header>
+      <main className=" grid lg:grid-cols-[218px_1fr] gap-14">
+        <div>
+          <SideBar />
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

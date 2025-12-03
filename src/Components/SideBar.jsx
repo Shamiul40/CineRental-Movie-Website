831d3740b7ef0a1cc63dc9bd140eaff1fd395def
Shamiul40 <shamiul4040@gmail.com>
2025-12-03 21:00:14 +0600

    data added

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import trending from "../assets/icons/trending.svg"
+import newRelease from "../assets/icons/newRelease.svg"
+import comingSoon from "../assets/icons/commingSoon.svg"
+import favourite from "../assets/icons/favourite.svg"
+import watchLater from "../assets/icons/watchLater.svg"
+
 
 export default function SideBar() {
   return (
@@ -6,31 +12,31 @@ export default function SideBar() {
 				<ul className="space-y-2">
 					<li>
 						<a className="flex items-center space-x-2 px-5 py-3.5 rounded-lg bg-primary text-black" href="#">
-							<img src="./assets/icons/trending.svg" width="24" height="24" alt="" />
+							<img src={trending} width="24" height="24" alt="trending" />
 							<span>Trending</span>
 						</a>
 					</li>
 					<li>
 						<a className="flex items-center space-x-2 px-5 py-3.5 rounded-lg" href="#">
-							<img src="./assets/icons/newRelease.svg" width="24" height="24" alt="" />
+							<img src={newRelease} width="24" height="24" alt="newrelease" />
 							<span>New Releases</span>
 						</a>
 					</li>
 					<li>
 						<a className="flex items-center space-x-2 px-5 py-3.5 rounded-lg" href="#">
-							<img src="./assets/icons/commingSoon.svg" width="24" height="24" alt="" />
+							<img src={comingSoon} width="24" height="24" alt="comingsoon" />
 							<span>Coming Soon</span>
 						</a>
 					</li>
 					<li>
 						<a className="flex items-center space-x-2 px-5 py-3.5 rounded-lg" href="#">
-							<img src="./assets/icons/favourite.svg" width="24" height="24" alt="" />
+							<img src={favourite} width="24" height="24" alt="favourite" />
 							<span>Favourites</span>
 						</a>
 					</li>
 					<li>
 						<a className="flex items-center space-x-2 px-5 py-3.5 rounded-lg" href="#">
-							<img src="./assets/icons/watchLater.svg" width="24" height="24" alt="" />
+							<img src={watchLater} width="24" height="24" alt="watchlater" />
 							<span>Watch Later</span>
 						</a>
 					</li>

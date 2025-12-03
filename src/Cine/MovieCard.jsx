bb9ffad69f02791dbd9231e4f3dd98bddb23dd78
Shamiul40 <shamiul4040@gmail.com>
2025-12-04 01:22:14 +0600

    rating component

--- src/Cine/MovieCard.jsx
+++ src/Cine/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getImageURL } from "../utility/utility";
+import Rating from './Rating';
 
 export default function MovieCard({movie}) {
   return (
@@ -15,12 +16,8 @@ export default function MovieCard({movie}) {
               <p className="text-[#575A6E] text-sm mb-2">
                 {movie.genre}
               </p>
-              <div className="flex items-center space-x-1 mb-5">
-                <img src="./assets/star.svg" width="14" height="14" alt="" />
-                <img src="./assets/star.svg" width="14" height="14" alt="" />
-                <img src="./assets/star.svg" width="14" height="14" alt="" />
-                <img src="./assets/star.svg" width="14" height="14" alt="" />
-                <img src="./assets/star.svg" width="14" height="14" alt="" />
+              <div className=" flex items-center space-x-1 mb-5">
+                <Rating value={movie.rating} />
               </div>
               <a
                 className="bg-green-400 rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
